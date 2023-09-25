c97fca10a35cb299eff7757b8724cdd4d6e8b6b0
hicircum <93832807+Hicircum@users.noreply.github.com>
2023-09-25 20:08:38 +0800

    feat: add sudoku components

--- sudoku/src/components/Sudoku.vue
+++ sudoku/src/components/Sudoku.vue
@@ -1,11 +1,148 @@
 <template>
-{{ problem }}
+    <div class="game-warp">
+      <div class="game-board">
+        <div class="game-inner">
+          <table class="game-table">
+            <tbody>
+              <tr v-for="(i, row) in problem" class="game-row">
+                <td
+                v-for="(x, col) in i"
+                class="game-col"
+                >
+                    <SudokuCell
+                    :cellNumber="x"
+                    />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import SudokuCell from './SudokuCell.vue'
 
 defineProps({
     problem: Array
 })
 </script>
+
+<style lang="less" scoped>
+.game-warp{
+  display: flex;
+  justify-content: center;
+}
+
+.game-board {
+    position: relative;
+    width: 100%;
+    max-width: 500px;
+    min-width: 250px;
+    background-color: #f3f6fa;
+    margin: 5px;
+
+    &::after {
+        content: '';
+        display: block;
+        padding-bottom: 100%;
+    }
+
+    .game-inner {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+}
+
+
+.game-table {
+    width: 100%;
+    margin: 0 auto;
+    padding: 0;
+    background: #ffffff;
+    user-select: none;
+    border: 2px solid #344861;
+  
+    &,
+    &::after {
+        display: block;
+        box-sizing: border-box;
+        height: 100%;
+    }
+
+    &::after {
+        content: '';
+        position: absolute;
+        left: calc(100% / 3);
+        width: calc(100% / 3);
+        top: 0;
+        border-left: 2px solid #344861;
+        border-right: 2px solid #344861;
+        pointer-events: none;
+    }
+  
+    tbody {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+  
+      &::after {
+        display: block;
+        content: '';
+        position: absolute;
+        left: 0;
+        top: calc(100% / 3);
+        height: calc(100% / 3);
+        border-top: 2px solid #344861;
+        border-bottom: 2px solid #344861;
+        pointer-events: none;
+      }
+    }
+}
+
+.game-row,
+.game-table tbody:after {
+    box-sizing: border-box;
+    width: 100%;
+}
+
+.game-row {
+    height: 11.11111%;
+}
+
+.game-col,
+.game-row {
+    display: flex;
+    padding: 0;
+    margin: 0;
+}
+
+.game-col {
+    flex-basis: 11.1111%;
+    box-sizing: border-box;
+    position: relative;
+    border-right: 1px solid #bec6d4;
+    border-bottom: 1px solid #bec6d4;
+    cursor: pointer;
+    transform: translateZ(0);
+    color: #080808;
+}
+
+.game-cover{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, 0.5);
+  z-index: 1;
+}
+</style>
---
+++ sudoku/src/components/SudokuCell.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="cell">
+        <div
+        class="cell-value"
+        >
+            <div class="cell-value-inner">
+                {{ handleZero(cellNumber) }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    cellNumber: Number,
+})
+
+function handleZero(num) {
+    if (num === 0) {
+        return ''
+    }else{
+        return num
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.cell {
+    width: 100%;
+    height: 100%;
+    .err::after{
+        position: absolute;
+        content: "";
+        right: 8%;
+        bottom: 8%;
+        width: 14%;
+        height: 14%;
+        background-color: #ff0000;
+        border-radius: 50%;
+    }
+    .problem{
+        background-color: #dadada;
+    }
+    .selected{
+        background-color: #caff8a;
+    }
+    .hover{
+        background-color: #caff8a !important;
+    }
+    .same{
+        background-color: #f2fde5 !important;
+    }
+    .user-input{
+        color: #5400c9 !important;
+    }
+    .cell-value {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        font-weight: 100;
+        text-align: center;
+        .cell-value-inner {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: calc(20px + 1vw);
+            font-weight: bold;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont,
+                        'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+                        'Helvetica Neue', sans-serif;
+        }
+    }
+}
+</style>
